c0b4027efc55e92f1b1f179cf04651fe6d137eb1
mishajib <mishajib222@gmail.com>
2020-09-23 00:22:09 +0600

    initial commit and adapter pattern added

---
+++ adapter_pattern.php
@@ -0,0 +1,107 @@
+<?php
+#Author - MI SHAJIB
+
+#=============================================
+#       PHP OOP - ADAPTER DESIGN PATTERN    #
+#=============================================
+
+interface SocialInterface
+{
+    public function getFriends();
+    public function getPosts();
+}
+
+class Facebook implements SocialInterface
+{
+    public function getFriends()
+    {
+        var_dump("Get all facebook friends");
+    }
+
+    public function getPosts()
+    {
+        var_dump("Get all facebook posts");
+    }
+}
+
+class Twitter
+{
+    public function getFollowers()
+    {
+        var_dump("Get twitter followers");
+    }
+
+    public function getTweets()
+    {
+        var_dump("Get all twitter tweets");
+    }
+}
+
+class TwitterAdapter implements SocialInterface
+{
+    protected $twitter;
+
+    public function __construct(Twitter $twitter)
+    {
+        $this->twitter = $twitter;
+    }
+
+    public function getFriends()
+    {
+        $this->twitter->getFollowers();
+    }
+
+    public function getPosts()
+    {
+        $this->twitter->getTweets();
+    }
+}
+
+class LinkedinAdapter implements SocialInterface
+{
+    protected $linkedin;
+
+    public function __construct(Linkedin $linkedin)
+    {
+        $this->linkedin = $linkedin;
+    }
+
+    public function getFriends()
+    {
+        $this->linkedin->getConnections();
+    }
+
+    public function getPosts()
+    {
+        $this->linkedin->getFeeds();
+    }
+}
+
+class Linkedin
+{
+    public function getConnections()
+    {
+        var_dump("Get all linkedin connections");
+    }
+
+    public function getFeeds()
+    {
+        var_dump("Get all linkeding feeds");
+    }
+}
+
+class Person
+{
+    public function social(SocialInterface $social)
+    {
+        $social->getFriends();
+        $social->getPosts();
+    }
+}
+
+$person = new Person;
+$person->social(new Facebook());
+echo PHP_EOL;
+$person->social(new TwitterAdapter(new Twitter));
+echo PHP_EOL;
+$person->social(new LinkedinAdapter(new Linkedin));
